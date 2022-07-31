2459bdc1c875339c0f5eea7b9b19a4a2970a83b9
Carl Madsen <madsencarlb@gmail.com>
2022-07-31 22:31:05 +1000

    add library.js data draw functionality

--- library.js
+++ library.js
@@ -2,18 +2,21 @@ let myLibrary = [
     {
         title: "1984",
         author: "George Orwell",
+        year: "5555",
         genre: "Science Fiction",
         haveRead: true,
     },
     {
         title: "Where the Wild Things Are",
         author: "Maurice Sendak",
+        year: "5555",
         genre: "Children's",
         haveRead: true,
     },
     {
         title: "A Scanner Darkly",
         author: "Philip K. Dick",
+        year: "5555",
         genre: "Science Fiction",
         haveRead: true,
     },
@@ -27,9 +30,16 @@ function addBookToLibrary() {
 
 }
 
-// let library = document.querySelector("#book-container");
-// for (let book of myLibrary){
-//     let bookBox = document.createElement("div");
-//     bookBox.textContent = `${book.title} by ${book.author}. ${book.genre}`;
-//     library.appendChild(bookBox);
-// }
+let library = document.querySelector("#book-container");
+for (let book of myLibrary){
+    let bookBox = document.createElement("div");
+    bookBox.classList.add("book");
+    for (let prop in book){
+        let propBox = document.createElement("div");
+        propBox.classList.add(`book-${prop}`);
+        propBox.textContent = book[prop];
+        bookBox.appendChild(propBox);
+    }
+    library.appendChild(bookBox);
+    library.appendChild(document.createElement("hr"))
+}
