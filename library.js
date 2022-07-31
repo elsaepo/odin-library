af0c254c34070ea58c6bcaca521fc72daeae6d07
Carl Madsen <madsencarlb@gmail.com>
2022-07-31 17:26:19 +1000

    set up library array and drawing it to the dom

--- library.js
+++ library.js
@@ -1,1 +1,33 @@
-console.log("library js")
+let myLibrary = [
+    {
+        title: "1984",
+        author: "George Orwell",
+        genre: "Science Fiction"
+    },
+    {
+        title: "Where the Wild Things Are",
+        author: "Maurice Sendak",
+        genre: "Children's"
+    },
+    {
+        title: "A Scanner Darkly",
+        author: "Philip K. Dick",
+        genre: "Science Fiction"
+    },
+];
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
+
+let library = document.querySelector("#library-container");
+for (let book in myLibrary){
+    book = myLibrary[book]
+    let bookBox = document.createElement("div");
+    bookBox.textContent = `${book.title} by ${book.author}. ${book.genre}`;
+    library.appendChild(bookBox);
+}
