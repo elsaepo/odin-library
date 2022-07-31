95eb3094a80f3f6305b2f0089f4c3c48a48cecfb
Carl Madsen <madsencarlb@gmail.com>
2022-07-31 22:18:40 +1000

    develop basic layout of page with placeholders

--- library.js
+++ library.js
@@ -2,17 +2,20 @@ let myLibrary = [
     {
         title: "1984",
         author: "George Orwell",
-        genre: "Science Fiction"
+        genre: "Science Fiction",
+        haveRead: true,
     },
     {
         title: "Where the Wild Things Are",
         author: "Maurice Sendak",
-        genre: "Children's"
+        genre: "Children's",
+        haveRead: true,
     },
     {
         title: "A Scanner Darkly",
         author: "Philip K. Dick",
-        genre: "Science Fiction"
+        genre: "Science Fiction",
+        haveRead: true,
     },
 ];
 
@@ -24,10 +27,9 @@ function addBookToLibrary() {
 
 }
 
-let library = document.querySelector("#library-container");
-for (let book in myLibrary){
-    book = myLibrary[book]
-    let bookBox = document.createElement("div");
-    bookBox.textContent = `${book.title} by ${book.author}. ${book.genre}`;
-    library.appendChild(bookBox);
-}
+// let library = document.querySelector("#book-container");
+// for (let book of myLibrary){
+//     let bookBox = document.createElement("div");
+//     bookBox.textContent = `${book.title} by ${book.author}. ${book.genre}`;
+//     library.appendChild(bookBox);
+// }
